6198af5eb4791a53e9aa6b21c3d5b73a25106ea7
tvytran <tvytran2@gmail.com>
2026-04-27 15:22:31 -0400

    used similar ray-casting method to find if coordinate is inside range, pull ranges in multiple areas ex. in features list or coordinates list

--- server.py
+++ server.py
@@ -4,7 +4,28 @@ import requests
 import smtplib
 from email.message import EmailMessage
 
+def coord_in_range(coord, box):
+    vertices = len(box)
+    x = coord[0]
+    y = coord[1]
+    inside = False
 
+    c1 = box[0]
+    #print("c1", c1[1])
+
+    for i in range(1, vertices+1):
+        c2 = box[i%vertices]
+
+        if y > min([c1[1],c2[1]]):
+            if y <= max(c1[1],c2[1]):
+                if x <= max(c1[0],c2[0]):
+                    x_intersection = (y-c1[1]) * (c2[0]-c1[0])/(c2[1]-c1[1]) + c1[0]
+
+                    if c1[0] == c2[0] or x <= x_intersection:
+                        inside = not inside
+        
+        c1 = c2
+    return inside
 
 if __name__ == '__main__':
     url = ["https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test/clinicianstatus/1", "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test/clinicianstatus/2", "https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test/clinicianstatus/3",
@@ -16,7 +37,36 @@ if __name__ == '__main__':
 
     for u in url:
         response = requests.get(u).json()
-        print(response['features'][0]['geometry']['coordinates'])
+        print("===============================")
+        if "error" in response:
+            print("An error has occured.")
+        else:
+            coord = response['features'][0]['geometry']['coordinates']
+        # print(type(coord[0]))
+
+            coordInRange = False
+            
+            wholeFeature = response['features']
+            for p in range(1, len(wholeFeature)):
+                box = response['features'][p]['geometry']['coordinates']
+                for b in box:
+                    print(b, "\n")
+                    if coord_in_range(coord,b):
+                        coordInRange = True
+            print(coordInRange)
+            #print(box)
+            #print(type(box))
+            #print(coord_in_range(coord,box))
+        
+    
+
+
+    
+
+
+    
+
+        
 
 
     '''
